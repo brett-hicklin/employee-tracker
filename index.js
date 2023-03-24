ddbd3964167bbc5d4f97f3956d2ab6bb0a42102f
Brett Hicklin <hicklin.brett@gmail.com>
2023-03-24 13:15:48 -0400

    corrected errors with the employee table

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const inquirer = require ('inquirer');
+const cTable = require('console.table');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'bootcamp',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the courses_db database.`)
+  );
+
 
 
 
